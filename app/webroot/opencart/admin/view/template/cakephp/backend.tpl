ddc14182a8ee15a8bddd62f4287e317e16d28550
antoine.wallef <anotine.wallef@gmail.com>
2013-04-09 19:44:06 +0200

    clear views

--- app/webroot/opencart/admin/view/template/cakephp/backend.tpl
+++
@@ -1,5 +0,0 @@
-<?php echo $header; ?>
-<div id="content">
-    <iframe src="<?php echo $iframe_url; ?>" width="100%" height="100%" style="border:0;"></iframe>
-</div>
-<?php echo $footer; ?>
--- app/webroot/opencart/catalog/controller/test/youpi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class ControllerTestYoupi extends Controller
-{ 	
-        public function index() {
-            $this->template = 'paloma/template/test/youpi.tpl';
-            $this->response->setOutput($this->render());
-        }
-}
-?>
